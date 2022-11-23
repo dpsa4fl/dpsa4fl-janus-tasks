d36a31103599d6fd15798746e1c87fa83121c3c2
Maxim Urschumzew <u.maxim@live.de>
2022-11-23 06:21:49 +0100

    Add package as flake output.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,42 @@
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustVersion;
+        rustc = rustVersion;
+      };
+
+      rustbuild_dpsa4fl-janus-tasks = rustPlatform.buildRustPackage {
+        pname =
+          "dpsa4fl-janus-tasks"; # make this what ever your cargo.toml package.name is
+        version = "0.1.0";
+        src = ./dpsa4fl-janus-tasks; # the folder with the cargo.toml
+        cargoLock.lockFile = ./dpsa4fl-janus-tasks/Cargo.lock;
+        cargoLock.outputHashes = {
+          "janus_aggregator-0.2.0" = "sha256-+mj6QwjfpAR92+0UoLJnnZGhKS4W66gELBNEHs86P/M=";
+        };
+        cargoBuildFlags = ""; #"-p janus_aggregator";
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [
+          pkgs.openssl
+        ];
+        doCheck = false;
+      };
+
     in {
+
+      packages = {
+        binary_dpsa4fl-janus-tasks = rustbuild_dpsa4fl-janus-tasks;
+      };
+      defaultPackage = rustbuild_dpsa4fl-janus-tasks;
+
       devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive pkgs.pkg-config ];
         buildInputs = with pkgs; [
